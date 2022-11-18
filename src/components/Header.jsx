c76918f9666ef27283a1677a9ba3a979ed4b01c5
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-18 20:25:48 +0100

    style: finished styling header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
-import { LeftSection } from "./styled/Header.styled";
-
+import { LeftSection, RightSection, StyledHeader } from "./styled/Header.styled";
+import menu from "../assets/menu.svg";
 const Header = () => {
   return (
-    <div>
-      <LeftSection>Hello</LeftSection>
-    </div>
+    <StyledHeader>
+      <LeftSection>StreetRates</LeftSection>
+      <RightSection>
+        <div></div>
+        <p>Home</p>
+        <p>Convert</p>
+        <p>News</p>
+        <p>Contact</p>
+      </RightSection>
+      <div>
+        <img src={menu} alt="menu" />
+      </div>
+    </StyledHeader>
   );
 };
 
--- src/components/styled/Header.styled.js
+++ src/components/styled/Header.styled.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
-export const LeftSection = styled.div`
+export const StyledHeader = styled.p`
   outline: 1px solid red;
+  background-color: #00296b;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1.6rem;
+`;
+
+export const LeftSection = styled.p`
+  outline: 1px solid green;
+  font-weight: 600;
+  font-size: 2.4rem;
+  line-height: 2.8rem;
+  color: #fff;
+`;
+
+export const RightSection = styled.div`
+  outline: 1px solid blue;
+  display: none;
 `;
