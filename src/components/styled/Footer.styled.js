cd9694529e66ec146b8dacfc2a963df1f6e3505d
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-19 05:29:09 +0100

    style:finished making footer responsive

--- src/components/styled/Footer.styled.js
+++ src/components/styled/Footer.styled.js
@@ -5,9 +5,17 @@ export const StyledFooter = styled.footer`
   margin-top: 11.97rem;
   color: #f8fafc;
   padding: 6.6rem 1.6rem 0 1.6rem;
+  @media (min-width: 1200px) {
+    margin: 0;
+    margin-top: 15rem;
+    padding: 5.6rem 12rem 0 12rem;
+  }
   div {
     outline: 1px solid red;
     text-align: center;
+    @media (min-width: 1200px) {
+      text-align: left;
+    }
     p {
       padding: 1rem 0;
       display: flex;
@@ -18,29 +26,56 @@ export const StyledFooter = styled.footer`
       font-size: 1.8rem;
       line-height: 2.4rem;
       margin-bottom: 4.2rem;
+      @media (min-width: 1200px) {
+        justify-content: flex-start;
+        margin-bottom: 1.6rem;
+      }
       img {
         width: 1.32rem;
+        @media (min-width: 1200px) {
+          display: none;
+        }
       }
     }
     ul {
       display: none;
+      @media (min-width: 1200px) {
+        display: block;
+        list-style: none;
+        font-weight: 400;
+        font-size: 1.4rem;
+        line-height: 2rem;
+        letter-spacing: 0.001em;
+        color: #f8fafc;
+      }
       li {
+        margin-bottom: 2.4rem;
       }
     }
   }
 `;
 
+export const GridContainer = styled.div`
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+  }
+`;
 export const FinalContainer = styled.section`
   display: flex;
   border-top: 1px solid #cbd5e1;
   align-items: center;
   justify-content: space-between;
   padding: 3.95rem 0;
-  p {
-  }
+
   p:nth-of-type(1) {
-    font-weight: 800;
     font-size: 1.8rem;
+    font-weight: 800;
     line-height: 2.4rem;
+  }
+  p:nth-of-type(2) {
+    font-size: 1.2rem;
+    font-weight: 400;
+    line-height: 1.8rem;
   }
 `;
