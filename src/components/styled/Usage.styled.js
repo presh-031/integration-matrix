b70fd63ed101bdd33a361aa98d9eef9004b6126f
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-19 04:20:36 +0100

    finished making usage section responsive

--- src/components/styled/Usage.styled.js
+++ src/components/styled/Usage.styled.js
@@ -2,14 +2,30 @@ import styled from "styled-components";
 
 export const StyledUsage = styled.section`
   margin: 4.8rem 3rem;
+  @media (min-width: 1200px) {
+    outline: 1px solid red;
+    margin: 0;
+    margin-top: 8rem;
+    margin: 8rem 22rem;
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 export const LeftSection = styled.div`
+  @media (min-width: 1200px) {
+    width: 40.5rem;
+  }
   p:nth-of-type(1) {
     color: #334155;
     font-weight: 700;
     font-size: 1.8rem;
     line-height: 2.4rem;
     margin-bottom: 0.8rem;
+    @media (min-width: 1200px) {
+      font-size: 2.4rem;
+      line-height: 3.2rem;
+      margin-bottom: 2.4rem;
+    }
   }
   p:nth-of-type(2) {
     color: #334155;
@@ -17,6 +33,12 @@ export const LeftSection = styled.div`
     font-size: 1.2rem;
     line-height: 1.8rem;
     margin-bottom: 2.4rem;
+    @media (min-width: 1200px) {
+      font-size: 1.4rem;
+      line-height: 2rem;
+      letter-spacing: 0.001em;
+      margin-bottom: 4rem;
+    }
   }
   ul {
     outline: 1px solid red;
@@ -35,7 +57,7 @@ export const LeftSection = styled.div`
       p {
         align-self: center;
         font-weight: 600;
-        font-size: 1.4rem;
+        font-size: 2rem;
         line-height: 1.8rem;
         outline: 1px solid red;
         letter-spacing: -0.02em;
@@ -57,12 +79,19 @@ export const LeftSection = styled.div`
 export const RightSection = styled.div`
   outline: 1px solid red;
   padding: 0;
-
+  @media (min-width: 1200px) {
+    display: grid;
+    place-items: center;
+  }
   img {
     width: 24.7rem;
     /* height: 28rem; */
     margin: 0 auto;
     display: block;
     outline: 1px solid blue;
+    @media (min-width: 1200px) {
+      width: 38rem;
+      height: 34rem;
+    }
   }
 `;
