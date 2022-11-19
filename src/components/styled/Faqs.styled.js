5ccacb3a5df9d0e0c765c922b11a3a2baeb2ce71
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-19 05:04:40 +0100

    finishd making faqs section responsive

--- src/components/styled/Faqs.styled.js
+++ src/components/styled/Faqs.styled.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 export const StyledFaqs = styled.section`
   outline: 1px solid red;
   margin-top: 4.8rem;
+  @media (min-width: 1200px) {
+    width: 72rem;
+    margin: 0 auto;
+    margin-top: 8rem;
+  }
 `;
 export const FaqsHeading = styled.p`
   text-align: center;
@@ -11,6 +16,11 @@ export const FaqsHeading = styled.p`
   line-height: 1.4rem;
   letter-spacing: -0.002em;
   margin-bottom: 1rem;
+  @media (min-width: 1200px) {
+    font-size: 2.8rem;
+    line-height: 3.2rem;
+    margin-bottom: 1.6rem;
+  }
 `;
 export const Question = styled.div`
   padding: 1.3rem 1.4rem;
@@ -19,15 +29,29 @@ export const Question = styled.div`
   border-bottom: 0.45px solid #c6c8d3;
   justify-content: space-between;
   align-items: center;
+  @media (min-width: 1200px) {
+    padding: 3.2rem 1rem 2.4rem 0;
+  }
   p {
     font-weight: 400;
     font-size: 1.2rem;
     line-height: 1.8rem;
     color: #334155;
+    @media (min-width: 1200px) {
+      font-size: 1.6rem;
+      line-height: 2.4rem;
+    }
   }
+  /* check logo to use another - most likely */
   img {
     width: 1.2rem;
     height: 1.2rem;
     display: block;
+    @media (min-width: 1200px) {
+      width: 2.4rem;
+      height: 2.4rem;
+      outline: 1px solid red;
+      object-fit: cover;
+    }
   }
 `;
